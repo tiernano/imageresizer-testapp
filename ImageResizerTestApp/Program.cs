64d9fa84f86b867eda252bc46c41630c1b024261
Tiernan OToole <tiernan@tiernanotoole.ie>
2018-07-02 18:58:42 +0100

    initial build of the app

--- ImageResizerTestApp/Program.cs
+++ ImageResizerTestApp/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using ImageSharp;
+using ImageSharp.Formats;
+using ImageSharp.Processing;
+using ImageSharp.Processing.Processors;
 
 namespace ImageResizerTestApp
 {
@@ -6,7 +13,52 @@ namespace ImageResizerTestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+	        string[] files = Directory.GetFiles(args[0], "*.jpg");
+
+	        Console.WriteLine($"Found {files.Length} files");
+	        
+
+			Parallel.ForEach(files, x =>
+	        {
+		        using (new AutoStopwatch($"Resizing {Path.GetFileName(x)}"))
+		        {
+			        string outputPath = Path.Combine(args[1], $"{Path.GetFileNameWithoutExtension(x)}.resized.jpg");
+			        using (var input = File.OpenRead(x))
+			        {
+				        using (var output = File.OpenWrite(outputPath))
+				        {
+					        using (Image<Rgba32> image = Image.Load(input))
+					        {
+						        image.Resize(1024, 1024);
+						        image.SaveAsJpeg(output);
+					        }
+					        output.Close();
+				        }
+				        input.Close();
+			        }
+		        }
+	        });
+
         }
     }
+
+	class AutoStopwatch : IDisposable
+	{
+		private Stopwatch mStopwatch;
+		private string mMessage;
+
+
+		public AutoStopwatch(string message)
+		{
+			mMessage = message;
+			mStopwatch = new Stopwatch();
+			mStopwatch.Start();
+		}
+
+		public void Dispose()
+		{
+			mStopwatch.Stop();
+			Console.WriteLine($"Finsihed {mMessage} in {mStopwatch.Elapsed}");
+		}
+	}
 }
